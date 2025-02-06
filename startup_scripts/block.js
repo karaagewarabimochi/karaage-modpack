f18e61c4c8919c4807f4319b436f6ef608ff6b60
karaagewarabimochi <hf3jd8@gmail.com>
2025-02-06 09:33:47 +0900

    サウンドの変更

--- startup_scripts/block.js
+++ startup_scripts/block.js
@@ -1,6 +1,6 @@
 StartupEvents.registry('block', event => {
-  event.create('superblock').textureAll('kubejs:block/superblock')
-  event.create('supersuperblock').textureAll('kubejs:block/supersuperblock')
+  event.create('superblock').textureAll('kubejs:block/superblock').stoneSoundType()
+  event.create('supersuperblock').textureAll('kubejs:block/supersuperblock').stoneSoundType()
   event.create('compressedcobblestone').textureAll('kubejs:block/compressed_cobblestone').stoneSoundType()
   event.create('bakedcompressedcobblestone').textureAll('kubejs:block/baked_compressed_cobblestone').stoneSoundType()
   event.create('testblock').textureAll('kubejs:block/testblock')
